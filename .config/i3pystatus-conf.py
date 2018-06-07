8d944dc70f4f27bedf059b645e12206eda1c5f18
Andrés Caro <andres.caro.q@gmail.com>
2018-06-07 14:35:49 -0400

    Add i3 and i3pystatus configs

---
+++ .config/i3pystatus-conf.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+from i3pystatus import Status
+
+status = Status(standalone=True)
+
+# Displays clock like this:
+# Tue 30 Jul 11:59:46 PM KW31
+#                          ^-- calendar week
+status.register("clock",
+                format="%a %-d %b %X KW%V",)
+
+# Shows the average load of the last minute and the last 5 minutes
+# (the default value for format is used)
+status.register("load")
+
+# Shows your CPU temperature, if you have a Intel CPU
+status.register("temp",
+                format="{temp:.0f}°C",)
+
+# Displays whether a DHCP client is running
+#status.register("runwatch",
+#    name="DHCP",
+#    path="/var/run/dhclient*.pid",)
+
+# Shows the address and up/down state of eth0. If it is up the address is shown in
+# green (the default value of color_up) and the CIDR-address is shown
+# (i.e. 10.10.10.42/24).
+# If it's down just the interface name (eth0) will be displayed in red
+# (defaults of format_down and color_down)
+#
+# Note: the network module requires PyPI package netifaces
+#status.register("network",
+#    interface="eth0",
+#    format_up="{v4cidr}",)
+
+# Has all the options of the normal network and adds some wireless specific things
+# like quality and network names.
+#
+
+# Shows disk usage of /
+# Format:
+# 42/128G [86G]
+status.register("disk",
+                path="/",
+                format="{used}/{total}GiB [{avail}GiB av]",)
+
+# Shows pulseaudio default sink volume
+#
+# Note: requires libpulseaudio from PyPI
+status.register("alsa",
+                format="🔊: {volume}%",)
+
+# Shows mpd status
+# Format:
+# Cloud connected▶Reroute to Remain
+status.register("mpd",
+                format = "{title} {status} {artist} ({album})",
+                max_field_len = 50,
+                on_upscroll = "previous_song",
+                on_downscroll = "next_song",
+                status = {
+                    "pause": "▷",
+                    "play": "▶",
+                    "stop": "◾",
+                },)
+
+status.run()
