6f53569007dd293ba6f8c0de7bff3373a4c0db1f
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-03-01 00:06:24 -0300

    feat(starfish): add starship prompt to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,8 @@
 set fish_greeting
-zoxide init fish | source
 set -U fish_user_abbreviations 'll=ls -la --group-directories-first'
 
-# Only if on Linux
-eval (keychain --agents ssh --eval --quiet id_rsa)
+zoxide init fish | source
+starship init fish | source
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
