7f360825038334971accfe348167edc176e898ff
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-12-12 23:54:18 -0300

    feat(nvim/telescope): add config to include dotfiles and a couple of keymaps

--- .config/nvim/lua/user/telescope.lua
+++ .config/nvim/lua/user/telescope.lua
@@ -3,16 +3,36 @@ if not status_ok then
   return
 end
 
+local status_ok_config, telescope_config = pcall(require, "telescope.config")
+if not status_ok_config then
+  return
+end
+
+local vimgrep_arguments = { unpack(telescope_config.values.vimgrep_arguments) }
+
+table.insert(vimgrep_arguments, "--hidden")
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
 local actions = require("telescope.actions")
 
 telescope.setup({
   defaults = {
+    vimgrep_arguments = vimgrep_arguments,
     mappings = {
       i = {
         ["<Esc>"] = actions.close,
         ["<C-j>"] = actions.move_selection_next,
         ["<C-k>"] = actions.move_selection_previous,
+        ["<C-b>"] = actions.results_scrolling_up,
+        ["<C-f>"] = actions.results_scrolling_down,
+        ["kj"] = actions.close,
       },
+    },
+  },
+  pickers = {
+    find_files = {
+      find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
     },
   },
 })
