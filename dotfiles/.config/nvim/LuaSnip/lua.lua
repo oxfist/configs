5c1c0c49179344dd7d92cd6ba784fa09d5904a95
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-12-30 23:45:18 -0300

    feat(nvim/luansip): add custom snippet for pcall on Lua and tweak config

---
+++ dotfiles/.config/nvim/LuaSnip/lua.lua
@@ -0,0 +1,50 @@
+local status_ok, ls = pcall(require, "luasnip")
+if not status_ok then
+  return
+end
+
+local s = ls.snippet
+-- local sn = ls.snippet_node
+-- local isn = ls.indent_snippet_node
+-- local t = ls.text_node
+local i = ls.insert_node
+-- local f = ls.function_node
+-- local c = ls.choice_node
+-- local d = ls.dynamic_node
+-- local r = ls.restore_node
+-- local events = require("luasnip.util.events")
+-- local ai = require("luasnip.nodes.absolute_indexer")
+local extras = require("luasnip.extras")
+-- local l = extras.lambda
+local rep = extras.rep
+-- local p = extras.partial
+-- local m = extras.match
+-- local n = extras.nonempty
+-- local dl = extras.dynamic_lambda
+-- local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+-- local conds = require("luasnip.extras.expand_conditions")
+-- local postfix = require("luasnip.extras.postfix").postfix
+-- local types = require("luasnip.util.types")
+-- local parse = require("luasnip.util.parser").parse_snippet
+
+return {
+  s(
+    {
+      trig = "pcall",
+      dscr = "Protected call for a Lua module for Neovim config",
+      regTrig = false,
+      -- snippetType = "autosnippet",
+      priority = 1000,
+    },
+    fmta(
+      [[
+        local <>, <> = pcall(require, "<>")
+        if not <> then
+          return
+        end<>
+      ]],
+      { i(1, "status_ok"), i(2), i(3), rep(1), i(0) }
+    )
+  ),
+}
--- dotfiles/.config/nvim/lua/user/luasnip.lua
+++ dotfiles/.config/nvim/lua/user/luasnip.lua
@@ -1,3 +1,14 @@
+-- Yes, we're just executing a bunch of Vimscript using vim.cmd
+vim.cmd([[
+  " Use Tab to expand and jump through snippets
+  imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
+  smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+
+  " Use Shift-Tab to jump backwards through snippets
+  imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+  smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+]])
+
 local status_ok, _ = pcall(require, "luasnip")
 if not status_ok then
   return
@@ -8,4 +19,12 @@ if not status_ok_vscode then
   return
 end
 
+local status_ok_lua, from_lua = pcall(require, "luasnip.loaders.from_lua")
+if not status_ok_lua then
+  return
+end
+
+from_lua.load({
+  paths = "~/.config/nvim/LuaSnip",
+})
 from_vscode.lazy_load()
