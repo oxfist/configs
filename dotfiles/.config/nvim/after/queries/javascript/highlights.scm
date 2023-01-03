afd9e32071893e3fb3fc3e44888a070c12d730ab
Andrés Quilodrán <andres.caro.q@gmail.com>
2023-01-03 00:27:01 -0300

    feat(nvim/treesitter): add queries for better JSX support

--- dotfiles/.config/nvim/after/queries/javascript/highlights.scm
+++ dotfiles/.config/nvim/after/queries/javascript/highlights.scm
@@ -1,27 +1,12 @@
 ;; extends
 
-; (import_clause
-;   (identifier) @include.identifier
-; )
-;
-; (named_imports
-;   (import_specifier
-;     name: (identifier) @include.identifier
-;   )
-; )
-
-(jsx_attribute
-  (jsx_expression
-    (member_expression
-      (identifier) @variable.object
-      (property_identifier) @variable.objectProperty
-    )
-  )
+(import_clause
+  (identifier) @include.identifier
 )
 
-(template_string
-  (template_substitution
-    (identifier) @variable.templateSubstitution
+(named_imports
+  (import_specifier
+    name: (identifier) @include.identifier
   )
 )
 
@@ -43,20 +28,41 @@
 
 operator: ("of") @operator.of
 
-(call_expression
-  function: (
-    member_expression
-      object: (member_expression
-        property: (property_identifier) @object.property
-      )
-      property: (property_identifier)
-  )
+(
+  member_expression 
+    object: (member_expression
+      property: (property_identifier) @object.property
+    )
+    property: (property_identifier)
 )
 
 (lexical_declaration
   "const"
   (variable_declarator
-    name: (identifier) @constant.identifier
-    (#set! "priority" 200)
+    [
+     name: (array_pattern
+              (identifier) @constant.identifier)
+     name: (object_pattern
+              (shorthand_property_identifier_pattern) @constant.identifier)
+     name: (identifier) @constant.identifier
+     (#set! "priority" 200)
+    ]
+))
+
+[(jsx_element
+   open_tag: (jsx_opening_element
+               name: (identifier) @tag.component.jsx
+               (#match? @tag.component.jsx "^[A-Z]")
+  ))
+ (jsx_self_closing_element
+  name: (identifier) @tag.component.jsx
+  (#match? @tag.component.jsx "^[A-Z]")
   )
-)
+]
+
+(class_declaration
+  body: (class_body
+          member: (field_definition
+                    property: (property_identifier) @class.field.function
+                    value: (arrow_function))))
+
